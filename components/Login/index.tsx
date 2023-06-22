b766eb81e3346f97702d092cc1efe37a4f655447
yedongCoding <yecoding@163.com>
2023-06-22 22:19:08 +0800

    登录弹框样式调整

--- components/Login/index.tsx
+++ components/Login/index.tsx
@@ -8,7 +8,7 @@ interface LoginProps {
 }
 
 const Login = (props: LoginProps) => {
-	const { isShow = false } = props;
+	const { isShow = false, onClose } = props;
 	const [isShowVerifyCode,setIsShowVerifyCode] = useState(false);
 
 	const [form, setForm] = useState({
@@ -17,7 +17,7 @@ const Login = (props: LoginProps) => {
 	});
 
 	const handleClose = () => {
-
+		onClose && onClose();
 	};
 
 	const handleGetVerifyCode = () => { 
@@ -37,9 +37,9 @@ const Login = (props: LoginProps) => {
 	};
 
 	return isShow ?
-		<div className={styles.loginArea}>
-			<div className={styles.loginBox}>
-				<div className={styles.loginTitle}>
+		<div className={styles.LoginArea}>
+			<div className={styles.LoginBox}>
+				<div className={styles.LoginTitle}>
 					<div>手机号登录</div>
 					<div className={styles.close} onClick={handleClose}>x</div>
 				</div>
@@ -50,7 +50,7 @@ const Login = (props: LoginProps) => {
 					value={form.phone}
 					onChange={handleFormChange}
 				/>
-				<div className={styles.verifyArea}>
+				<div className={styles.VerifyArea}>
 					<input
 						name="verify"
 						type="text"
@@ -58,12 +58,12 @@ const Login = (props: LoginProps) => {
 						value={form.verify}
 						onChange={handleFormChange}
 					/>
-					<span className={styles.verifyCode} onClick={handleGetVerifyCode}>
+					<span className={styles.VerifyCode} onClick={handleGetVerifyCode}>
 						{isShowVerifyCode? <CountDown time={10} onEnd={handleCountDownEnd} /> : '获取验证码'}
 					</span>
 				</div>
-				<div className={styles.loginBtn} onClick={handleLogin}>登录</div>
-				<div className={styles.otherLogin} onClick={handleOAuthLogin}>Github登录</div>
+				<div className={styles.LoginBtn} onClick={handleLogin}>登录</div>
+				<div className={styles.OtherLogin} onClick={handleOAuthLogin}>Github登录</div>
 				<div className={styles.privacy} >
 					注册登录即表示同意
 					<a href='https://moco.imooc.com/privacy.html' target='_blank' rel='noreferrer'>隐私政策</a>
